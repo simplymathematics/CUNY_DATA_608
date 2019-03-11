f0b0ec928860ccc1704fcf8c0497810231d32258
simplymathematics <charliemeyers@gmail.com>
2019-03-10 22:59:00 -0400

    + app2

--- module3/app2/app.R
+++ module3/app2/app.R
@@ -1,11 +1,11 @@
 df <- read.csv('https://raw.githubusercontent.com/charleyferrari/CUNY_DATA_608/master/module3/data/cleaned-cdc-mortality-1999-2010-2.csv')
 df <- as.data.frame(df)
-
+df <- as.data.frame(subset(df, df$Year == '2010'))
+df
 ui <- fluidPage(
   headerPanel('Death Rates by Various Cause for each State'),
   sidebarPanel(
-    selectInput('disease', 'Disease', unique(df$ICD.Chapter), selected='Certain conditions originating in the perinatal period'),
-    selectInput('year', 'Year', unique(df$Year), selected='2010')
+    selectInput('disease', 'Disease', unique(df$ICD.Chapter), selected='Certain conditions originating in the perinatal period')
   ),
   mainPanel(
     plotOutput('plot1')
@@ -16,22 +16,19 @@ server <- shinyServer(function(input, output, session) {
   
   selectedData <- reactive({
     dfSlice <- df %>%
-      filter(ICD.Chapter == input$disease, Year == input$year)
+      filter(ICD.Chapter == input$disease)
   })
   
   output$plot1 <- renderPlot({
     
     dfSlice <- df %>%
-      filter(ICD.Chapter == input$disease, Year == input$year)
-    dfSlice <- as.data.frame(dfSlice)
+      filter(ICD.Chapter == input$disease)
     
     ggplot(selectedData(), aes(x = State, y = Crude.Rate)) +
-      geom_bar(stat = 'identity') +
-      geom_hline(yintercept=mean(dfSlice$Crude.Rate), linetype="dashed", color = "red", show.legend = TRUE) + 
-      geom_hline(yintercept=median(dfSlice$Crude.Rate), linetype="dashed", color = "blue", show.legend = TRUE) +
-      labs(caption = "Blue line reflects national average. Red line reflects national median.")
+      geom_bar(stat = 'identity')
   })
   
   
 })
+
 shinyApp(ui = ui, server = server)
