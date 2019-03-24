89b159d67751cedf3175c6eaf569659f75f6e6f2
simplymathematics <charliemeyers@gmail.com>
2019-03-24 03:28:03 +0000

    app 2

--- module4/app.py
+++ module4/app.py
@@ -7,6 +7,10 @@ import flask
 import pandas as pd
 import time
 import os
+import numpy as np
+import plotly
+import plotly.graph_objs as go
+import scipy.stats as scs
 
 server = flask.Flask('app')
 server.secret_key = os.environ.get('secret_key', 'secret')
@@ -25,7 +29,134 @@ for boro in boros:
     soql_health['boroname'] = boro
     city = city.append(soql_health)
 
+# With steward, Good Heatlth
 
+city2 = pd.DataFrame()
+boros = ["'Bronx'","'Staten Island'", "'Manhattan'", "'Queens'", "'Brooklyn'"]
+for boro in boros:
+    soql_url2 = ('https://data.cityofnewyork.us/resource/nwxe-4ae8.json?' +\
+        '$select=steward, count(health)' +\
+        '&$where=boroname='+boro +\
+        'AND NOT health=\'NaN\'' +\
+        'AND NOT steward=\'NaN\'' +\
+        'AND NOT steward=\'None\'' +\
+        'AND NOT health=\'Poor\'' +\
+        '&$group=steward').replace(' ', '%20')
+    soql_steward = pd.read_json(soql_url2)
+    soql_steward['boroname'] = boro
+    city2 = city2.append(soql_steward)
+city2 = city2.replace('3or4', 1)
+city2 = city2.replace('4orMore', 1)
+city2 = city2.replace('1or2', 1)
+
+city2 = city2.groupby('boroname').sum()
+city2 = city2.drop(columns = 'steward')
+city2
+
+# No Steward, Good Health
+
+city3 = pd.DataFrame()
+boros = ["'Bronx'","'Staten Island'", "'Manhattan'", "'Queens'", "'Brooklyn'"]
+for boro in boros:
+    soql_url2 = ('https://data.cityofnewyork.us/resource/nwxe-4ae8.json?' +\
+        '$select=steward, count(health)' +\
+        '&$where=boroname='+boro +\
+        'AND NOT health=\'NaN\'' +\
+        'AND steward=\'None\'' +\
+        'AND NOT steward=\'NaN\'' +\
+        'AND NOT health=\'Poor\'' +\
+        '&$group=steward').replace(' ', '%20')
+    soql_steward = pd.read_json(soql_url2)
+    soql_steward['boroname'] = boro
+    city3 = city3.append(soql_steward)
+
+city3 = city3.replace({'None': 0})
+city3 = city3.groupby('boroname').sum()
+city3 = city3.drop(columns = 'steward')
+city3
+
+# With steward, Bad Health
+
+city4 = pd.DataFrame()
+boros = ["'Bronx'","'Staten Island'", "'Manhattan'", "'Queens'", "'Brooklyn'"]
+for boro in boros:
+    soql_url2 = ('https://data.cityofnewyork.us/resource/nwxe-4ae8.json?' +\
+        '$select=steward, count(health)' +\
+        '&$where=boroname='+boro +\
+        'AND NOT health=\'NaN\'' +\
+        'AND NOT steward=\'NaN\'' +\
+        'AND NOT steward=\'None\'' +\
+        'AND health=\'Poor\'' +\
+        '&$group=steward').replace(' ', '%20')
+    soql_steward = pd.read_json(soql_url2)
+    soql_steward['boroname'] = boro
+    city4 = city4.append(soql_steward)
+city4 = city4.replace('3or4', 1)
+city4 = city4.replace('4orMore', 1)
+city4 = city4.replace('1or2', 1)
+
+city4 = city4.groupby('boroname').sum()
+city4 = city4.drop(columns = 'steward')
+city4
+
+# No Steward, Bad Health
+
+city5 = pd.DataFrame()
+boros = ["'Bronx'","'Staten Island'", "'Manhattan'", "'Queens'", "'Brooklyn'"]
+for boro in boros:
+    soql_url2 = ('https://data.cityofnewyork.us/resource/nwxe-4ae8.json?' +\
+        '$select=steward, count(health)' +\
+        '&$where=boroname='+boro +\
+        'AND NOT health=\'NaN\'' +\
+        'AND steward=\'None\'' +\
+        'AND NOT steward=\'NaN\'' +\
+        'AND health=\'Poor\'' +\
+        '&$group=steward').replace(' ', '%20')
+    soql_steward = pd.read_json(soql_url2)
+    soql_steward['boroname'] = boro
+    city5 = city5.append(soql_steward)
+
+city5 = city5.replace({'None': 0})
+city5 = city5.groupby('boroname').sum()
+city5 = city5.drop(columns = 'steward')
+city5
+
+# Make big df for analysis
+
+steward_good = city2
+steward_bad = city4
+
+no_good = city3
+no_bad = city5
+big = pd.DataFrame()
+
+steward_good
+
+
+big['steward good'] = steward_good['count_health']
+big['steward bad']  = steward_bad['count_health']
+big['no steward good'] = no_good['count_health']
+big['no steward bad']  = no_bad['count_health']
+
+big = big.transpose()
+
+# Statistics Test
+
+obs = [big[boro][0], big[boro][1] ]
+exp = [big[boro][2], big[boro][3] ]
+
+obs_total = np.sum(obs)
+exp_total = np.sum(exp)
+
+obs = obs/obs_total * 100
+exp = exp/exp_total * 100
+
+result = scs.chisquare(obs,exp)
+result = bool(result[1]<.05)
+if result == True:
+    conclusion = "We have sufficient evidence to suggest that stewardship effects tree health."
+else:
+    conclusion = "We have insufficient evidence to suggest that stewardship effects tree health."
 app = dash.Dash('app', server=server)
 
 app.scripts.config.serve_locally = False
@@ -44,9 +175,25 @@ app.layout = html.Div([
         ],
         value="'Queens'"
     ),
-    dcc.Graph(id='my-graph')
+    dcc.Graph(id='my-graph'),
+    html.H1('Tree Health vs Stewardship'),
+    dcc.Dropdown(
+        id='my-dropdown2',
+        options=[
+            {'label': 'Queens', 'value': "'Queens'"},
+            {'label': 'Brooklyn', 'value': "'Brooklyn'"},
+            {'label': 'Manhattan', 'value': "'Manhattan'"},
+            {'label': 'Staten Island', 'value': "'Staten Island'"},
+            {'label': 'Bronx', 'value': "'Bronx'"}
+        ],
+        value="'Queens'"
+    ),
+    dcc.Graph(id='total-graph'),
+    html.H2("Conclustion: "),
+    html.H3(conclusion)
 ], className="container")
 
+#interactive graph
 @app.callback(Output('my-graph', 'figure'),
               [Input('my-dropdown', 'value')])
 def update_graph(selected_dropdown_value):
@@ -58,6 +205,34 @@ def update_graph(selected_dropdown_value):
             'y': dff.count_tree_id,
             'type': 'bar'
         }],
+        'layout': {
+            'margin': {
+                'l': 30,
+                'r': 20,
+                'b': 30,
+                't': 20
+            }
+        }
+    }
+
+
+# graph 2
+@app.callback(Output('total-graph', 'figure'),
+              [Input('my-dropdown2', 'value')])
+def update_graph2(selected_dropdown_value):
+    boro = selected_dropdown_value
+    trace1 = go.Bar(
+        x=["Good Health", "Bad Health"],
+        y= [big[boro][0], big[boro][1]],
+        name='With Steward'
+    )
+    trace2 = go.Bar(
+        x= ["Good Health", "Bad Health"],
+        y= [big[boro][2], big[boro][3]],
+        name='Without Steward'
+    )
+    return {
+        'data': [trace1, trace2],
         'layout': {
             'margin': {
                 'l': 30,
